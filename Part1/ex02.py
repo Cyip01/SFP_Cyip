1d518785e4ba9d3ea34223306c5b80d4bf20e8a3
Cyip01 <25085267@imail.sunway.edu.my>
2025-07-21 05:46:21 +0000

    done

---
+++ Part1/ex02.py
@@ -0,0 +1,2 @@
+name = "chloe"
+print(name)
---
+++ Part1/ex03.py
@@ -0,0 +1,17 @@
+# Original text
+string = 'MaRviN THE bOt'
+
+# convert to lowercase
+string_lower = string_lower()
+
+# convert to uppercase 
+string_upper = string_upper()
+
+# remove spaces
+string_replace = string_replace(chr(32), '')
+
+# Print results
+print(string)
+print(string_lower)
+print(string_upper)
+print(string_replace)
